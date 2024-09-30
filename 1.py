5b704989777bd3442f3422a691e34dde082079f7
mengxianze <mengxianze@yljr.com>
2024-09-30 14:23:37 +0800

    error

--- 1.py
+++ 1.py
@@ -1,78 +1,166 @@
-Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x000002A0ACDFE280>
+========================================
+[Success] Convert to images: file<合同超期3.pdf> in contract<contract1>
+[Success] Convert to images: file<长沙越大设备管道安装合同(1).pdf> in con
+
+========================================
+=== Step 3: Correct Images ===
+========================================
+[Success] No need to be corrected: file<合同超期3.pdf> in contract<contra
+Failed with exception: File is not a zip file
+Failed with exception: Server disconnected
+Failed with exception: Server disconnected
+Failed with exception: Server disconnected
+Failed with exception: Server disconnected
+Error occurred: cannot unpack non-iterable bool object
 Traceback (most recent call last):
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 116, in 
+  File "C:\Users\gpu009\contract\contarct_file.py", line 73, in correct_i
+    self.geo_images, self.mapping_matrix = future.result()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\concur result
+    return self.__get_result()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\concur __get_result
+    raise self._exception
+  File "C:\Users\gpu009\contract\process\p3_1_correct_img\doctr_api.py", 
+    results = await asyncio.gather(*tasks)
+  File "C:\Users\gpu009\contract\process\p3_1_correct_img\doctr_api.py", 
+    correct_bytes, bms = await doctr_api_async(session, img_bytes)
+TypeError: cannot unpack non-iterable bool object
+
+
+
+
+
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
 __del__
     self.close()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 108, in 
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
 close
     self._loop.call_soon(self._call_connection_lost, None)
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 751, in call_soon
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncick_closed
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    self._loop.call_soon(self._call_connection_lost, None)
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncick_closed
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    self._loop.call_soon(self._call_connection_lost, None)
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncick_closed
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    self._loop.call_soon(self._call_connection_lost, None)
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncick_closed
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    self._loop.call_soon(self._call_connection_lost, None)
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncick_closed
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    self._loop.call_soon(self._call_connection_lost, None)
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncick_closed
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncick_closed
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    raise RuntimeError('Event loop is closed')
+RuntimeError: Event loop is closed
+Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x0
+Traceback (most recent call last):
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+__del__
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    self.close()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci
+close
+    self._loop.call_soon(self._call_connection_lost, None)
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
+_soon
+_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci_soon
     self._check_closed()
   File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 515, in _check_closed
     raise RuntimeError('Event loop is closed')
 RuntimeError: Event loop is closed
-Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x000002A0ACDFE280>
-Traceback (most recent call last):
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 116, in 
-__del__
-    self.close()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 108, in 
-close
-    self._loop.call_soon(self._call_connection_lost, None)
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 751, in call_soon
+_soon
+    self._check_closed()
+_soon
+_soon
     self._check_closed()
   File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 515, in _check_closed
+_soon
+    self._check_closed()
+  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asynci  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 515, in _check_closed
+o\base_events.py", line 515, in _check_closed
     raise RuntimeError('Event loop is closed')
-RuntimeError: Event loop is closed
-Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x000002A0ACDFE280>
-Traceback (most recent call last):
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 116, in 
-__del__
-    self.close()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 108, in 
-close
-    self._loop.call_soon(self._call_connection_lost, None)
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 751, in call_soon
-    self._check_closed()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 515, in _check_closed
-    raise RuntimeError('Event loop is closed')
-RuntimeError: Event loop is closed
-Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x000002A0ACDFE280>
-Traceback (most recent call last):
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 116, in 
-__del__
-    self.close()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 108, in 
-close
-    self._loop.call_soon(self._call_connection_lost, None)
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 751, in call_soon
-    self._check_closed()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 515, in _check_closed
-    raise RuntimeError('Event loop is closed')
-RuntimeError: Event loop is closed
-Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x000002A0ACDFE280>
-Traceback (most recent call last):
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 116, in 
-__del__
-    self.close()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 108, in 
-close
-    self._loop.call_soon(self._call_connection_lost, None)
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 751, in call_soon
-    self._check_closed()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 515, in _check_closed
-    raise RuntimeError('Event loop is closed')
-RuntimeError: Event loop is closed
-Exception ignored in: <function _ProactorBasePipeTransport.__del__ at 0x000002A0ACDFE280>
-Traceback (most recent call last):
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 116, in 
-__del__
-    self.close()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\proactor_events.py", line 108, in 
-close
-    self._loop.call_soon(self._call_connection_lost, None)
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 751, in call_soon
-    self._check_closed()
-  File "C:\Users\gpu009\AppData\Local\Programs\Python\Python39\lib\asyncio\base_events.py", line 515, in _check_closed
     raise RuntimeError('Event loop is closed')
 RuntimeError: Event loop is closed
